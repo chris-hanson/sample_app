4e43ef6b890242cec9f0437e2228228e353e85bd
Christopher Hanson <chris.f.hanson@gmail.com>
2013-03-12 19:24:48 +1030

    add contact page and title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleAppV2::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  match '/help'    => 'static_pages#help'
+  match '/about'   => 'static_pages#about'
+  match '/contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +53,7 @@ SampleAppV2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+  subject { page }
+
+  describe "Home" do
+    it "should have content 'Sample App'" do
+      visit root_path
+      should have_selector('h1', text: 'Sample App')
+    end
+  end
+
+  describe "Help" do
+    it "should have content 'Help'" do
+      visit help_path
+      should have_selector('h1', text: 'Help')      
+    end
+  end
+
+  describe "About" do
+    it "should have content 'About'" do
+      visit about_path
+      should have_selector('h1', text: 'About')
+    end
+  end
+
+  describe "Contact" do
+    it "should have content 'Contact'" do
+      visit contact_path
+      should have_selector('h1', text: 'Contact')
     end
   end
 end
