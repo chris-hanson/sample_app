bada609357a5a7a1c4e409d90df07e3a3a702f56
Christopher Hanson <chris.f.hanson@gmail.com>
2013-03-14 20:24:45 +1030

    Start on signup page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,41 @@ describe "UserPages" do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }
-    it { should have_link('Create my account') }
+    it { should have_button('Create my account') }
+    
+  end
+
+  describe "profile page" do
+
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+  end
+
+  describe "sinup" do
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        old_count = User.count
+        click_button "Create my account"
+        User.count.should == old_count
+      end
+    end
+
+    describe "with valid information" do
+      it "should create a user" do
+        fill_in "Name", with: "example user"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+
+        old_count = User.count
+        click_button "Create my account"
+        User.count.should == old_count +1
+      end
+    end
     
   end
 end
