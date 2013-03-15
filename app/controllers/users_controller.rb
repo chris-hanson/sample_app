6670328b33b87e3170a83da05c6fe9a640eb12b4
Christopher Hanson <chris.f.hanson@gmail.com>
2013-03-16 01:30:33 +1030

    Hide unneeded elements from signed_in? user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def show
@@ -8,14 +12,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new params[:user]
-    
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome, your account hsa been created!"
-      redirect_to @user
+    if signed_in?
+      redirect_to root_path
     else
-      render 'new'
+      @user = User.new params[:user]
+      
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome, your account hsa been created!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 end
